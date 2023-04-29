5e20564a2560747d219035ca073de3d4030c429d
Yash <Yash@DESKTOP-LLCENBA>
2023-04-29 14:15:08 +0530

    Pages Added

---
+++ MVCProject/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            return View();
+        }
+    }
+}
--- MVCProject/Controllers/ProductController.cs
+++ MVCProject/Controllers/ProductController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
 
 namespace MVCProject.Controllers
 {
     public class ProductController : Controller
     {
         [HttpGet]
-        public IActionResult Add()
+        public IActionResult Add(Product product)
         {
             return View();
         }
