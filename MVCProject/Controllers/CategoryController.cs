b65a2040a7f5472cb649742ae0d64327536f61db
diksha <dikshadawande00@gmail.com>
2023-04-29 18:50:06 +0530

    gh

--- MVCProject/Controllers/CategoryController.cs
+++ MVCProject/Controllers/CategoryController.cs
@@ -1,18 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCProject.Data;
 using MVCProject.Models;
 
-namespace MVCProject.Controllers
-{
+namespace MVCProject.Controllers {
     public class CategoryController : Controller
     {
-        [HttpGet]
-        public IActionResult GetCategory()
+        private readonly ContextFile _context;
+        public CategoryController(ContextFile context)
         {
-            return View();
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCategory()
+        {
+            var catList = await _context.Categories.ToListAsync();
+            return View(catList);
         }
         [HttpPost]
-        public IActionResult AddCategory(Category category)
+        public async Task<IActionResult> AddCategory(Category category)
         {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return View();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var ab = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+             _context.Categories.Remove(ab);
+            _context.SaveChanges()
+;
             return View();
         }
     }
